390678a4b7fcfe4018f09bb0710ad57b51670bde
dixiNourmus <joshua.berger@talawanda.org>
2024-02-07 13:49:30 -0500

    meow

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,31 @@
 public class Main {
     public static void main(String[] args) {
+        int OperandA = 2;
+        int OperandB = 5;
+        int Sum = OperandA + OperandB;
+        int Product = OperandA * OperandB;
+        int Difference = OperandA - OperandB;
+        int Quotient = OperandA/OperandB;
+        int Modulo = OperandA%OperandB;
 
-        System.out.println("Hello world!");
+        double dblOperandA = 2;
+        double dblOperandB = 5;
+        double dblSum = dblOperandA + dblOperandB;
+        double dblProduct = dblOperandA * dblOperandB;
+        double dblDifference = dblOperandA - dblOperandB;
+        double dblQuotient = dblOperandA/dblOperandB;
+        double dblModulo = dblOperandA%dblOperandB;
 
-
+        System.out.println("Sum: "+ Sum);
+        System.out.println("Product: " + Product);
+        System.out.println("difference " + Difference);
+        System.out.println("quotient " + Quotient);
+        System.out.println("modulo " + Modulo);
+        System.out.println("double Sum: "+ dblSum);
+        System.out.println("double Product: " + dblProduct);
+        System.out.println("double difference " + dblDifference);
+        System.out.println("double quotient " + dblQuotient);
+        System.out.println("double modulo " + dblModulo);
 
 
 

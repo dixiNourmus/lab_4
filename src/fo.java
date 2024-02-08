938a602c45ec4c107d957ac69f8994475928c040
dixiNourmus <joshua.berger@talawanda.org>
2024-02-08 13:54:55 -0500

    meow

---
+++ src/fo.java
@@ -0,0 +1,12 @@
+public class fo {
+    public static void main(String[] args) {
+        int num1 = 2;
+        if (num1%2==0){
+            System.out.println("your number is even");
+        }
+        else {
+            System.out.println("your number is odd");
+        }
+
+    }
+}

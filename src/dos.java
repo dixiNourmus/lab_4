0a0edda4a06b9bb792858e92233255112466937c
dixiNourmus <joshua.berger@talawanda.org>
2024-02-08 13:44:18 -0500

    meow

---
+++ src/dos.java
@@ -0,0 +1,19 @@
+public class dos {
+    public static void main(String[] args) {
+
+        int spring = 12678990;
+        int summer = 187658752;
+        int fall = 656588;
+        int winter = 6969;
+        double total = spring + summer + fall + winter;
+
+        System.out.println("your total maintence is " + total);
+
+
+
+
+    }
+
+
+
+}

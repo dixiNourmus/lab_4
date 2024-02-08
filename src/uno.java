9a2e54e639a0350f46b1f7212ef12fdde5cd3cd3
dixiNourmus <joshua.berger@talawanda.org>
2024-02-08 13:36:27 -0500

    meow

---
+++ src/uno.java
@@ -0,0 +1,16 @@
+public class uno {
+    public static void main(String[] args) {
+
+        double salesTax = 0.05;
+        int price = 87;
+        double total = price * salesTax + price;
+
+        System.out.println("your total is " + total);
+        System.out.println("your tax is " + price * salesTax);
+
+
+
+
+
+    }
+}
